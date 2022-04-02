884c73a04b2dbfcea6fdc5b99028481d15184127
G_Anta <ganta@telecomboutique.com>
2022-04-02 14:05:39 +0200

    keyBoard component && global state

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,44 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import SimpleKeyboard from "./components/SimpleKeyboard.vue";
+import { onMounted, reactive } from "vue";
+
+const KEY_CODE_MAPPING = {
+  13: "{enter}",
+  8: "{bksp}"
+};
+
+const state = reactive({
+  solution: "books",
+  guesses: ["", "", "", "", "", ""],
+  guessIndex: 0,
+})
+
+function handleInput(key) {
+  console.log(key);
+}
+
+function getKey({ keyCode }) {
+  console.log(typeof keyCode, keyCode);
+  return  keyCode in KEY_CODE_MAPPING
+    ? KEY_CODE_MAPPING[keyCode]
+    : String.fromCharCode(keyCode).toLowerCase();
+}
+
+onMounted(() => {
+  window.addEventListener("keyup", (e) => {
+    e.preventDefault();
+    let key = getKey(e);
+    handleInput(key);
+  });
+});
+
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="flex flex-col h-screen max-w-md mx-auto justify-evently">
+    <simple-keyboard @onKeyPress="handleInput"/>
+  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/SimpleKeyboard.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="simple-keyboard"></div>
+</template>
+
+<script setup>
+import Keyboard from "simple-keyboard";
+import "simple-keyboard/build/css/index.css";
+import { ref, onMounted } from "vue";
+
+const layout = {
+  default: [
+    "q w e r t y u i o p",
+    "a s d f g h j k l ñ",
+    "{enter} z x c v b n m {bksp}"
+  ]
+}
+
+const keyboard = ref(null);
+const emit = defineEmits(["onKeyPress"]);
+const onKeyPress = button => emit("onKeyPress", button);
+
+onMounted(() => {
+    keyboard.value = new Keyboard("simple-keyboard", { layout, onKeyPress: onKeyPress });
+});
+</script>
+
+<style></style>
