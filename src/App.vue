bdcbd1eee33443dc9ada836e6d4f6c5b78eefa18
G_Anta <ganta@telecomboutique.com>
2022-04-04 16:56:54 +0200

    working wordRow and letterBox && dynamic word length

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,79 @@
 <script setup>
 import SimpleKeyboard from "./components/SimpleKeyboard.vue";
+import WordRow from "./components/WordRow.vue";
+import LetterBox from "./components/LetterBox.vue";
 import { onMounted, reactive } from "vue";
 
 const KEY_CODE_MAPPING = {
-  13: "{enter}",
-  8: "{bksp}"
+    13: "{enter}",
+    8: "{bksp}",
 };
 
+const WINNING_WORD = "palabras";
+
 const state = reactive({
-  solution: "books",
-  guesses: ["", "", "", "", "", ""],
-  guessIndex: 0,
-})
+    solution: WINNING_WORD,
+    guesses: new Array(WINNING_WORD.length).fill(""),
+    guessIndex: 0,
+    usedLetters: {
+        miss: [],
+        found: [],
+        hint: [],
+    },
+});
+
+function isValidKey(key) {
+    const alphaRegex = /^[a-zA-Z]+$/;
+    return alphaRegex.test(key);
+}
+
+const isInSolution = (c) => state.solution.indexOf(c) != -1;
 
 function handleInput(key) {
-  console.log(key);
+    if (state.guessIndex >= 6) return;
+
+    const currentGuess = state.guesses[state.guessIndex];
+
+    if (key === "{enter}" && currentGuess.length === state.solution.length) {
+        state.guessIndex++;
+        for (let index in currentGuess) {
+            let c = currentGuess.charAt(index);
+            if (c === state.solution.charAt(index)) state.usedLetters.found.push(c);
+            if (isInSolution(c)) state.usedLetters.hint.push(c);
+            if (!isInSolution(c)) state.usedLetters.miss.push(c);
+        }
+    }
+    if (key === "{bksp}") state.guesses[state.guessIndex] = currentGuess.slice(0, -1);
+    if (currentGuess.length < state.solution.length && isValidKey(key)) state.guesses[state.guessIndex] += key;
 }
 
 function getKey({ keyCode }) {
-  console.log(typeof keyCode, keyCode);
-  return  keyCode in KEY_CODE_MAPPING
-    ? KEY_CODE_MAPPING[keyCode]
-    : String.fromCharCode(keyCode).toLowerCase();
+    return keyCode in KEY_CODE_MAPPING ? KEY_CODE_MAPPING[keyCode] : String.fromCharCode(keyCode).toLowerCase();
 }
 
 onMounted(() => {
-  window.addEventListener("keyup", (e) => {
-    e.preventDefault();
-    let key = getKey(e);
-    handleInput(key);
-  });
+    window.addEventListener("keyup", (e) => {
+        e.preventDefault();
+        let key = getKey(e);
+        console.log(e.keyCode);
+        handleInput(key);
+    });
 });
-
 </script>
 
 <template>
-  <div class="flex flex-col h-screen max-w-md mx-auto justify-evently">
-    <simple-keyboard @onKeyPress="handleInput"/>
-  </div>
+    <div class="flex flex-col h-screen max-w-md mx-auto justify-evently">
+        <div>
+            <word-row
+                v-for="(guess, index) in state.guesses"
+                :key="index"
+                :value="guess"
+                :solution="state.solution"
+                :submitted="index < state.guesses"
+            />
+        </div>
+        <simple-keyboard @onKeyPress="handleInput" :usedLetters="state.usedLetters" />
+    </div>
 </template>
 
-<style>
-</style>
+<style></style>
---
+++ src/components/WordRow.vue
@@ -0,0 +1,35 @@
+<script setup>
+import LetterBox from "./LetterBox.vue";
+import { watch, ref, onMounted } from "vue";
+
+const props = defineProps({
+    value: String,
+    solution: String,
+    submitted: Boolean
+});
+
+const colores = ref(new Array(5).fill("gray"));
+
+
+/* onMounted(() => {
+    const rows = document.querySelectorAll(".grid");
+    console.log("props.solution.length",props.solution.length, `grid-cols-${props.solution.length}`);
+    rows.forEach(row => row.classList.add(`grid-cols-${props.solution.length}`))
+}); */
+
+watch(
+    () => props.submitted,
+    async (submitted, prevSubmitted) => {
+        if(!props.submitted) return;
+        let { solution, value } = props;
+        let tempColors = new Array(5).fill("gray");
+    }
+);
+
+</script>
+
+<template>
+    <div class="grid max-w-xs gap-1 mx-auto mb-1" :class="[`grid-cols-${props.solution.length}`]">
+        <letter-box v-for="index in props.solution.length" :key="index" :letter="value[index-1]"/>
+    </div>
+</template>
